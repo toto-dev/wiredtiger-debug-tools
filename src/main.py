dd6405aa2096edec122b38530a6337ec25e5def1
Tommaso Tocci <tommaso.tocci@mongodb.com>
2024-06-29 14:13:13 +0200

    Fix format with ruff

--- src/main.py
+++ src/main.py
@@ -17,22 +17,25 @@ WT_SUFFIX = ".wt"
 
 logger = logging.getLogger(__name__)
 
+
 def add_suffix(path, suffix):
     if path.suffix == suffix:
         return path
     return path.with_suffix(path.suffix + suffix)
 
+
 def json_converter(o):
     if isinstance(o, (datetime.datetime, datetime.date)):
         return o.isoformat()
-    #elif isinstance(o, UUID):
-    #    return o.hex
     else:
         return str(o)
 
+
 def stream_wt_table(tableName):
-    return subprocess.Popen(["wt", "-R", "dump", "-x", tableName],
-            stdout=subprocess.PIPE).stdout
+    return subprocess.Popen(
+        ["wt", "-R", "dump", "-x", tableName], stdout=subprocess.PIPE
+    ).stdout
+
 
 def load_wt_table(tableName):
     stream = stream_wt_table(tableName)
@@ -44,7 +47,7 @@ def load_wt_table(tableName):
         if not line:
             raise Exception(f"Couldn't find data header in '{tableName}' wt table")
         line = line.strip()
-        if line == b'Data':
+        if line == b"Data":
             break
 
     while True:
@@ -53,21 +56,23 @@ def load_wt_table(tableName):
             break
         key_raw = key_raw.strip()
         value_raw = stream.readline().strip()
-        value_string = codecs.decode(value_raw, 'hex')
+        value_string = codecs.decode(value_raw, "hex")
         value = bson.loads(value_string)
         arr.append(value)
 
     return arr
+
 
 def convert_table(table_name, output_file=None):
     if output_file is None:
         output_file = add_suffix(Path(table_name), JSON_SUFFIX)
 
     table_decoded = load_wt_table(table_name)
-    with open(output_file, 'w') as f:
+    with open(output_file, "w") as f:
         json.dump(table_decoded, f, default=json_converter)
     logger.info(f"Converted table '{table_name}' to '{output_file}")
     return table_decoded, output_file
+
 
 def load_catalog():
     catalog_path = add_suffix(Path(CATALOG_TABLE_NAME), JSON_SUFFIX)
@@ -77,10 +82,11 @@ def load_catalog():
 
     return convert_table(CATALOG_TABLE_NAME)[0]
 
+
 def get_coll_ident(coll_name):
     for e in load_catalog():
-        if e['ns'] == coll_name:
-            return e['ident']
+        if e["ns"] == coll_name:
+            return e["ident"]
     raise Exception(f"Collection '{coll_name}' not found in the catalog")
 
 
@@ -108,31 +114,36 @@ def load_collection(name):
     logging.debug(f"Found ident '{ident}' for collection '{coll_name}'")
     return convert_table(ident, json_file)
 
+
 @click.group()
 def cli():
     pass
 
+
 @cli.command()
 def list_collections():
     for e in load_catalog():
-        ns = e['ns']
-        ident = e['ident']
-        click.echo(click.style(ns, fg='green') + " " + click.style(f"({ident})"))
+        ns = e["ns"]
+        ident = e["ident"]
+        click.echo(click.style(ns, fg="green") + " " + click.style(f"({ident})"))
 
-@cli.command('convert')
-@click.argument('coll_name')
+
+@cli.command("convert")
+@click.argument("coll_name")
 def convert_cmd(coll_name):
     data, file = load_collection(coll_name)
     click.echo(f"Converted collection '{coll_name}' to '{file}'")
 
-@cli.command('cat')
-@click.argument('coll_name')
+
+@cli.command("cat")
+@click.argument("coll_name")
 def cat_cmd(coll_name):
     data, file = load_collection(coll_name)
 
     console = Console()
     with console.pager(styles=True):
         console.print_json(json.dumps(data, default=json_converter))
+
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
